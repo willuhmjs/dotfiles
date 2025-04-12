7ccdf625bd6aca07bff173f0b0e7d03730e1ea96
willuhmjs <willuhmjs@gmail.com>
2025-04-11 22:51:15 -0400

    kitty transparency

--- .config/setup.sh
+++ .config/setup.sh
@@ -2,4 +2,4 @@
 yay -Syu hyprland firefox lf waybar grimblast-git wl-clipboard zsh hyprpaper neovim zathura tesseract-data-eng zathura-pdf-mupdf bluez bluez-utils
 sudo rm -rf /etc/xdg/waybar/
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
+xdg-mime default org.pwmt.zathura.desktop application/pdf
