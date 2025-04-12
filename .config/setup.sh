c5a49ade5a31ff69d145b9cf02ebdf08d6ee66ed
willuhmjs <willuhmjs@gmail.com>
2025-04-12 08:56:42 -0400

    add kitty to install script

--- .config/setup.sh
+++ .config/setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-yay -Syu hyprland firefox lf waybar grimblast-git wl-clipboard zsh hyprpaper neovim zathura tesseract-data-eng zathura-pdf-mupdf bluez bluez-utils
+yay -Syu hyprland firefox lf waybar kitty grimblast-git wl-clipboard zsh hyprpaper neovim zathura tesseract-data-eng zathura-pdf-mupdf bluez bluez-utils
 sudo rm -rf /etc/xdg/waybar/
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 xdg-mime default org.pwmt.zathura.desktop application/pdf
